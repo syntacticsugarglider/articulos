9165dd390da6430300ff3c0e9024ff57d7aed0c1
Jacob Swart <zenerboson@gmail.com>
2019-02-11 15:32:49 -0800

    Another refactor

--- src/components/EditableContent.vue
+++ src/components/EditableContent.vue
@@ -1,90 +1,100 @@
 <template lang="pug">
-component.content(contenteditable, :is="element")
+component.content(:contenteditable="contentType !== 3", :is="element")
   template(v-for="item in content.entries()")
-    EditableContent(v-if="typeof item[1] !== 'string'", :ref="item[0]", :data-key="item[0]", :key="item[0]")
-    span(contenteditable, v-else, v-text="item[1]", :ref="item[0]", :data-key="item[0]", :key="item[0]")
+    span(contenteditable, v-if="typeof item[1] === 'string'", v-text="item[1]", :ref="item[0]", :data-key="item[0]")
+    br(v-else-if="item[1] === 1", :data-key="item[0]")
+    EditableContent(v-else, :ref="item[0]", :data-key="item[0]", v-model="item[1]")
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 enum Type {
-  Paragraph = 1,
+  Editor = 1,
+  Paragraph,
   Header,
 }
 
+enum Identity {
+  LineBreak = 1,
+}
+
 interface Item {
-  content: Array<Item | string>;
+  content: Array<Item | Identity | string>;
   type: Type;
 }
 
 const ElementForType = new Map<Type, string>();
 
 ElementForType.set(Type.Paragraph, 'p');
+ElementForType.set(Type.Editor, 'div');
 
 export default Vue.extend({
   name: 'EditableContent',
+  props: ['value'],
   data() {
+    const type = this.$props.value ? (this.$props.value as Item).type : Type.Editor;
     return {
-      element: 'p',
-      content: [] as Array<Item | string>,
-      type: Type.Paragraph,
+      element: ElementForType.get(type),
+      content: this.$props.value ? (this.$props.value as Item).content :
+        [{content: [String.fromCharCode(65279)], type: Type.Paragraph}] as Array<Item | string>,
+      contentType: type,
     };
   },
   mounted() {
     const ZWNBS = String.fromCharCode(65279);
-    this.appendContent(ZWNBS);
-    new MutationObserver((E) => {
+    const mo = new MutationObserver((E) => {
       E.forEach((e) => {
         if (e.type === 'characterData') {
-          if ((e.target.parentElement as HTMLElement).isSameNode(this.$el)) {
-            this.appendContent(e.target.nodeValue!);
-            (e.target as Element).remove();
-            this.$nextTick(() => {
-              this.moveCarat(e.target.nodeValue!.length);
-            });
-            return;
-          }
-          this.content[parseInt((e.target.parentElement as HTMLElement).getAttribute('data-key')!, 10)] =
-            e.target.nodeValue!;
+          this.content[parseInt(e.target.parentElement!.getAttribute('data-key')!, 10)]
+            = e.target.nodeValue!;
           if (e.target.nodeValue!.includes(ZWNBS)) {
             e.target.nodeValue = e.target.nodeValue!.replace(ZWNBS, '');
-            this.moveCarat(1);
+            this.moveCaret(1);
           }
         } else if (e.type === 'childList') {
-          e.removedNodes.forEach((el) => {
-            if (el.nodeType === 3) {
-              return;
-            }
-            this.content.splice(parseInt((el as any).attributes.getNamedItem('data-key'), 10), 1);
-          });
-          e.addedNodes.forEach((el) => {
-            if (el.nodeName === 'BR' && el.parentElement!.firstChild!.isSameNode(el)) {
-              (el as Element).remove();
+          e.addedNodes.forEach((node) => {
+            if (node.nodeName === 'BR' && node.previousSibling && node.previousSibling!.nodeName === 'BR') {
+              e.target.parentElement!.appendChild(document.createElement('br'));
+              this.content.push(Identity.LineBreak);
+              this.content.push(ZWNBS);
+              this.$nextTick(() => {
+                mo.observe((this.$refs[this.content.length - 1] as Node[])[0], {
+                  characterData: true,
+                  childList: true,
+                  subtree: true,
+                });
+              });
             }
           });
         }
       });
+    });
+    for (const item of this.content.entries()) {
+      if (typeof item[1] === 'string') {
+        mo.observe((this.$refs[item[0]] as Node[])[0], {
+          characterData: true,
+          childList: true,
+          subtree: true,
+        });
+      }
+    }
+    new MutationObserver((E) => {
+      E.forEach((e) => {
+        console.log(e);
+      });
     }).observe(this.$el, {
       childList: true,
-      characterData: true,
-      subtree: true,
     });
-    this.focus();
   },
   methods: {
-    focus() {
-      (this.$el as HTMLElement).focus();
+    focusAtEnd() {
+      const sel = document.getSelection()!;
+      sel.collapse((this.$refs[this.content.length - 1] as Node).lastChild!, 1);
     },
-    appendContent(content: Item | string) {
-      this.content.push(content);
-    },
-    moveCarat(offset: number) {
+    moveCaret(offset: number) {
       const sel = document.getSelection()!;
       sel.collapse(sel.anchorNode, sel.anchorOffset + offset);
-    },
-    insertContent(index: number, content: Item | string) {
-      this.content.splice(index, 0, content);
     },
   },
 });
